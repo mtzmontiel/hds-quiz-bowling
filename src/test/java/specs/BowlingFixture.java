ffe8e2596cf724eb4b716c21e3ce3a60e7f76872
Rafael Martínez Montiel <rafael.martinez@saludhds.com.mx>
2013-12-27 14:56:27 -0600

    Especificacion de entradas a jugar

--- src/test/java/specs/BowlingFixture.java
+++ src/test/java/specs/BowlingFixture.java
@@ -2,7 +2,11 @@ package specs;
 
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
+
 @RunWith(ConcordionRunner.class)
 public class BowlingFixture {
 
+	public int framesFor(int players) {
+		return players * 10;
+	}
 }
