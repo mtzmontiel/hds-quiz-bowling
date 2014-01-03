dc315af7e2da20293a409c9bf968cec943a44629
Rafael Martínez Montiel <rafael.martinez@saludhds.com.mx>
2014-01-03 16:07:20 -0600

    Introducir interfaz Entrada

--- src/test/java/specs/bowling/tiros/TirosPorEntradaStrikeEntrada10Tiro2Fixture.java
+++ src/test/java/specs/bowling/tiros/TirosPorEntradaStrikeEntrada10Tiro2Fixture.java
@@ -3,14 +3,14 @@ package specs.bowling.tiros;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-import com.hds.arq.quizz.bowling.EntradaEstandar;
+import com.hds.arq.quizz.bowling.Entrada;
 import com.hds.arq.quizz.bowling.EntradaFactory;
 
 @RunWith(ConcordionRunner.class)
 public class TirosPorEntradaStrikeEntrada10Tiro2Fixture {
 
     public int tirosDisponibles(int primero, int segundo) {
-        final EntradaEstandar entrada = EntradaFactory.nuevaEntrada();
+        final Entrada entrada = EntradaFactory.nuevaEntrada();
         entrada.tiro(primero);
         entrada.tiro(segundo);
         return entrada.tirosDisponibles();
--- src/test/java/specs/bowling/tiros/TirosPorEntradaStrikeEntrada10Tiro3Fixture.java
+++ src/test/java/specs/bowling/tiros/TirosPorEntradaStrikeEntrada10Tiro3Fixture.java
@@ -3,14 +3,14 @@ package specs.bowling.tiros;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-import com.hds.arq.quizz.bowling.EntradaEstandar;
+import com.hds.arq.quizz.bowling.Entrada;
 import com.hds.arq.quizz.bowling.EntradaFactory;
 
 @RunWith(ConcordionRunner.class)
 public class TirosPorEntradaStrikeEntrada10Tiro3Fixture {
 
     public int tirosDisponibles(int primero, int segundo, int tercero) {
-        final EntradaEstandar entrada = EntradaFactory.nuevaEntrada();
+        final Entrada entrada = EntradaFactory.nuevaEntrada();
         entrada.tiro(primero);
         entrada.tiro(segundo);
         entrada.tiro(tercero);
